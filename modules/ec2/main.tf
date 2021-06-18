06720df0840c929128404ac9efa990940a40ec6f
root <root@ip-172-31-4-97.ap-south-1.compute.internal>
2021-06-18 09:10:36 +0000

    initial commit

---
+++ modules/ec2/main.tf
@@ -0,0 +1,21 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ec2_instance" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ec2_instance.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2 Demo Instance"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "region"
+  default = "ap-south-1"
+}
