2cfd31e55ebfba60af6c3e0d02f8d87245c35f79
Ashish Pandey <tech4abetterfuture@gmail.com>
2021-06-18 15:11:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1
+   access_key  = "AKIAVASHO7M4UMQ6Y7XK"
+   secret_key  = "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"
   profile = "prashant_appgambit"
 }
