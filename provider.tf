d5f9fa746719f8e6c7f31c8450ff11ae78efa012
Ashish Pandey <tech4abetterfuture@gmail.com>
2021-07-02 14:03:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-   access_key  = "AKIAVASHO7M4UMQ6Y7XK"
-   secret_key  = "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"
+  shared_credentials_file = "/Users/david/.aws/credentials"
+  profile = "testing"
 }
